07c145c4864b1a5d7a9d655a32fcbe4f10b6e446
Malix - Alix Brunet <alixbrunetcontact@gmail.com>
2026-01-13 19:56:04 +0100

    fix: hash routing

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -4,10 +4,11 @@ import React from 'react';
 export const Link = React.forwardRef<
     HTMLAnchorElement,
     React.AnchorHTMLAttributes<HTMLAnchorElement>
->(({ className, children, ...props }, ref) => (
+>(({ className, children, href, ...props }, ref) => (
     <a
         ref={ref}
         className={cn('text-pink-600 hover:underline', className)}
+        href={href?.startsWith('/') ? `#${href}` : href}
         {...props}
     >
         {children}
